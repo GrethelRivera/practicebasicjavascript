76d2db86131ab4887ea026586a62b887cf3664df
Rivera <GRivera@nfpa.org>
2022-05-25 18:08:20 -0400

    first commit

---
+++ myList.js
@@ -0,0 +1,14 @@
+//Create a shopping List
+
+const myList = [
+    ["Mixed Greens", 1],
+    ["Cherry Tomato", 1],
+    ["Blue Cheese Crumbles", 1],
+    ["Bacon Bits", 1],
+    ["Candied Pecans", 1],
+    ["Avacado", 1],
+    ["Seasoned Croutons", 1],
+    ["Balsamic Vinaigrette", 1 ]
+    ]
+    
+    console.log(myList)
